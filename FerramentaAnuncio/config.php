11fc0316884e7e28bd06e3faead6abfe6e5fff08
Gabriel Godoy da Silva <110994759+ogabrielgodoy@users.noreply.github.com>
2022-11-24 12:36:22 -0300

    Add files via upload

---
+++ FerramentaAnuncio/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$erro=true;
+$largura = 1200;
+$altura = 1200;
+$logo = 'logo.gif';
+$diretorio = 'arquivo/';
+
+//formatos
+// $jpg = "image/jpeg" && "image/pjpeg";
+// $png = "image/png" && "image/x-png";
+// $webp = "image/webp";
+?>
